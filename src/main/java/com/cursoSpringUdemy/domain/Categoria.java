09445ee3d2103916b23b9b7239b50272222a62c5
adeliovdlk <adelioa.c@gmail.com>
2020-12-07 08:37:38 -0300

    tBanco de dados h2e criarcao automatica da base de dados

--- src/main/java/com/cursoSpringUdemy/domain/Categoria.java
+++ src/main/java/com/cursoSpringUdemy/domain/Categoria.java
@@ -1,9 +1,15 @@
 package com.cursoSpringUdemy.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
-
+@Entity
 public class Categoria implements Serializable {
     public static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//geracao de chave primaria
     private int id;
     private String name;//parei aki
 
